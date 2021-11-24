013a77aa5c7f6d36b25af81bc05c5d2a5b0e5395
Ahmet <ahmt9417@gmail.com>
2021-11-24 18:09:48 +0300

    Style detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,88 +1,68 @@
 import { useSelector } from 'react-redux';
-// import { v4 as uuidv4 } from 'uuid';
 import { Link } from 'react-router-dom';
 import { IoIosArrowBack, IoMdMic, IoMdSettings } from 'react-icons/io';
 import { HiOutlineArrowCircleRight } from 'react-icons/hi';
 import store from '../redux/configureStore';
 import getWorldData from '../redux/API';
+import './Details.css';
 
 const Details = () => {
   const state = useSelector((state) => state.covidData);
 
   return (
-    <div>
-      <div className="d-flex">
+    <div className="text-light fw-normal fs-5">
+      <div className="d-flex justify-content-between pink2 p-4 fs-5 fw-light">
         <Link to="/">
           <IoIosArrowBack
+            className="text-light"
             onClick={() => {
               store.dispatch(getWorldData(''));
             }}
           />
         </Link>
-        <p>country details</p>
-        <IoMdMic />
-        <IoMdSettings />
-      </div>
-
-      <div>
-        <p>{state.Country_text}</p>
-        <p>{state['Last Update']}</p>
-      </div>
-      {/* <div>
-        {Object.entries(state).map(([key1, value]) => (
-          <div key={uuidv4()}>
-            <b>{key1}</b>
-            <b>{value}</b>
-            <span>
-              {value}
-            </span>
-            <span>
-              <HiOutlineArrowCircleRight />
-            </span>
-          </div>
-        ))}
-      </div> */}
-      <div>
-        <div className="d-flex justify-content-between">
-          <b>New Case</b>
-          <div>
-            <span>
-              {state['New Deaths_text'] === '' ? 0 : state['New Deaths_text']}
-            </span>
-            <span>
-              <HiOutlineArrowCircleRight />
-            </span>
-          </div>
-        </div>
-        <div className="d-flex justify-content-between">
-          <b>Total Case</b>
-          <div>
-            <span>{state['Total Cases_text']}</span>
-            <span>
-              <HiOutlineArrowCircleRight />
-            </span>
-          </div>
-        </div>
-        <div className="d-flex justify-content-between">
-          <b>Total Death</b>
-          <div>
-            <span>{state['Total Deaths_text']}</span>
-            <span>
-              <HiOutlineArrowCircleRight />
-            </span>
-          </div>
-        </div>
-        <div className="d-flex justify-content-between">
-          <b>Total Recovered</b>
-          <div>
-            <span>{state['Total Recovered_text']}</span>
-
-            <span>
-              <HiOutlineArrowCircleRight />
-            </span>
-          </div>
+        <span>country details</span>
+        <div>
+          <IoMdMic />
+          <IoMdSettings className="ms-3" />
         </div>
       </div>
+      <ul className="list-unstyled fw-bold">
+        <li className="p-4 py-5 text-end justify-content-end pink ">
+          <p>{state.Country_text}</p>
+          <span className="fw-light">Last Update</span>
+          <p className="fw-light">
+            {state['Last Update']}
+          </p>
+        </li>
+        <li className="d-flex justify-content-between p-4 pink2 ">
+          <span>New Case</span>
+          <div className="fw-normal">
+            {state['New Deaths_text'] === '' ? 0 : state['New Deaths_text']}
+            <HiOutlineArrowCircleRight className="ms-3" />
+          </div>
+        </li>
+        <li className="d-flex justify-content-between p-4 pink">
+          <span>Total Case</span>
+          <div className="fw-normal">
+            {state['Total Cases_text']}
+            <HiOutlineArrowCircleRight className="ms-3" />
+          </div>
+        </li>
+        <li className="d-flex justify-content-between p-4 pink2">
+          <span>Total Death</span>
+          <div className="fw-normal">
+            {state['Total Deaths_text']}
+            <HiOutlineArrowCircleRight className="ms-3" />
+          </div>
+        </li>
+        <li className="d-flex justify-content-between p-4 pink">
+          <span>Total Recovered</span>
+          <div className="fw-normal">
+            {state['Total Recovered_text']}
+            <HiOutlineArrowCircleRight className="ms-3" />
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
